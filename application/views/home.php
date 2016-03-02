3a329ed4c257081a8414911f9020f7a78ca6da59
Hameedullah Pardess <hameedullah.pardess@gmail.com>
2016-03-03 03:44:14 +0430

    Initial Project Setup

---
+++ application/views/home.php
@@ -0,0 +1,11 @@
+<html>
+
+	<head>
+		<?php $this->load->view('meta'); ?>
+		
+		</head>
+	<body>
+		<?php $this->load->view('menu_bar'); ?>
+		<?php $this->load->view('footer'); ?>
+	</body>
+</html>
---
+++ application/views/meta.php
@@ -0,0 +1,9 @@
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.css"); ?>" />
+
+
+<!-- <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro|Open+Sans+Condensed:300|Raleway' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" type="text/css" media="all" href="<?php echo base_url(); ?>css/style.css" /> -->
+
